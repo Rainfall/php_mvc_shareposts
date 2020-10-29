c75b6bbcb6196160521c61056b6ec68a23a00e71
Denis Raygoroskiy <paradoxetion@gmail.com>
2020-10-29 11:47:15 +0300

    Posts model

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -2,8 +2,14 @@
 	
 	class Posts extends Controller {
 
+		public $some = 'hi';
+
 		public function __construct(){
-		 
+		 	echo $this->some;
+		}
+
+		public function index() {
+			$this->view('posts/index');
 		}
 
 
